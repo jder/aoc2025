09f310cb076cd638a5188cdf892e6ac15e3066e9
Jesse Rusak <me@jesserusak.com>
2025-12-01 09:46:01 -0500

    part 2

--- src/day01.rs
+++ src/day01.rs
@@ -12,7 +12,7 @@ pub fn part1(input: &str, _is_sample: bool) -> usize {
             "R" => 1,
             _ => panic!("unknown direction {letter}"),
         };
-        now = (now + num_st.parse::<isize>().unwrap() * dir) % 100;
+        now = (now + num_st.parse::<isize>().unwrap() * dir).rem_euclid(100);
         now
     });
 
@@ -20,5 +20,27 @@ pub fn part1(input: &str, _is_sample: bool) -> usize {
 }
 
 pub fn part2(input: &str, _is_sample: bool) -> usize {
-    todo!()
+    let mut now: isize = 50;
+    let zeros = input.lines().map(|line| {
+        let (letter, num_st) = line.split_at(1);
+        let dir = match letter {
+            "L" => -1,
+            "R" => 1,
+            _ => panic!("unknown direction {letter}"),
+        };
+        let distance = num_st.parse::<isize>().unwrap();
+        let loops = distance / 100;
+        let next = (now + distance * dir).rem_euclid(100);
+
+        let extras = if (dir.signum() != (next - now).signum() || next == 0) && now != 0 {
+            1
+        } else {
+            0
+        };
+
+        now = next;
+        loops + extras
+    });
+
+    zeros.sum::<isize>() as usize
 }

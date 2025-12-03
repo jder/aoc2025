8e6eada8cdd5aa109341c7a74b4f559c30010e68
Jesse Rusak <me@jesserusak.com>
2025-12-03 13:12:39 -0500

    day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -15,20 +15,27 @@ fn digit_to_val(digit: Char) -> usize {
     digit.to_char().to_digit(10).unwrap() as usize
 }
 
-fn largest_joltage(haystack: &[Char]) -> usize {
-    let biggest_index = largest_index(&haystack[..haystack.len() - 1]);
-    let next_index = largest_index(&haystack[biggest_index + 1..]) + biggest_index + 1;
+fn largest_joltage(haystack: &[Char], batteries: usize) -> usize {
+    if batteries == 0 {
+        0
+    } else {
+        let biggest_index = largest_index(&haystack[..haystack.len() - batteries + 1]);
+        let rest = largest_joltage(&haystack[biggest_index + 1..], batteries - 1);
 
-    digit_to_val(haystack[biggest_index]) * 10 + digit_to_val(haystack[next_index])
+        digit_to_val(haystack[biggest_index]) * 10usize.pow(batteries as u32 - 1) + rest
+    }
 }
 
 pub fn part1(input: &str, _is_sample: bool) -> usize {
     input
         .lines()
-        .map(|line| largest_joltage(line.as_ascii().unwrap()))
+        .map(|line| largest_joltage(line.as_ascii().unwrap(), 2))
         .sum()
 }
 
 pub fn part2(input: &str, _is_sample: bool) -> usize {
-    todo!()
+    input
+        .lines()
+        .map(|line| largest_joltage(line.as_ascii().unwrap(), 12))
+        .sum()
 }

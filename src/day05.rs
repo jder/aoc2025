7bd53b7d1d809a3e4d9010bf09c57db390884030
Jesse Rusak <me@jesserusak.com>
2025-12-05 09:20:22 -0500

    day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -33,6 +33,34 @@ pub fn part1(input: &str, _is_sample: bool) -> usize {
         .count()
 }
 
+fn union(a: &RangeInclusive<usize>, b: &RangeInclusive<usize>) -> Option<RangeInclusive<usize>> {
+    (a.end() >= b.start() && a.start() <= b.end())
+        .then(|| *a.start().min(b.start())..=*(a.end().max(b.end())))
+}
+
 pub fn part2(input: &str, _is_sample: bool) -> usize {
-    todo!()
+    let (ranges, _candidates) = parser().parse(input).unwrap();
+
+    let disjoint_ranges = ranges.into_iter().fold(vec![], |accum, mut range| {
+        // The newly-added range may overlap with multiple existing ranges,
+        // so we remove any which overlap and continue to expand this one to cover
+        // all overlaps.
+        let accum = accum.into_iter().flat_map(|maybe_overlapping| {
+            if let Some(union) = union(&maybe_overlapping, &range) {
+                range = union;
+                None
+            } else {
+                Some(maybe_overlapping)
+            }
+        });
+
+        let mut result = accum.collect_vec();
+        result.push(range);
+        result
+    });
+
+    disjoint_ranges
+        .iter()
+        .map(|range| range.end() - range.start() + 1)
+        .sum()
 }

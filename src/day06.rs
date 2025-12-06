59cf7c9d941e1625bc24c84734d6a29f4b5b32ba
Jesse Rusak <me@jesserusak.com>
2025-12-06 11:23:52 -0500

    part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,7 +1,4 @@
-use std::{
-    iter,
-    ops::{Add, Mul},
-};
+use std::ops::{Add, Mul};
 
 #[allow(unused)]
 use crate::prelude::*;
@@ -31,43 +28,32 @@ pub fn part1(input: &str, _is_sample: bool) -> usize {
         .sum()
 }
 
-fn transpose<'l>(lines: impl Iterator<Item = &'l str>) -> impl Iterator<Item = String> {
-    let lines = lines.map(|l| l.as_bytes()).collect_vec();
-    (0..lines.first().unwrap().len()).map(move |index| {
-        String::from_utf8(
-            lines
-                .iter()
-                .map(|line| line[index])
-                .chain(iter::once(' ' as u8))
-                .collect_vec(),
-        )
-        .unwrap()
-    })
-}
-
 pub fn part2(input: &str, _is_sample: bool) -> usize {
     let mut lines = input.lines();
-    let numbers = transpose(lines.take_while_ref(|line| !line.starts_with(&['*', '+'])))
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|num| num.parse().unwrap())
-                .collect_vec()
-        })
+    let numbers = lines
+        .take_while_ref(|line| !line.starts_with(&['*', '+']))
+        .map(|line| line.as_bytes())
         .collect_vec();
-
-    println!("{numbers:?}");
-
     let ops = lines
         .next()
         .unwrap()
-        .split_ascii_whitespace()
-        .map(|c| match c {
-            "+" => <usize as Add>::add,
-            "*" => <usize as Mul>::mul,
-            _ => panic!("unknown op {c}"),
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| match c {
+            '+' => Some((i, <usize as Add>::add as fn(usize, usize) -> usize)),
+            '*' => Some((i, <usize as Mul>::mul as fn(usize, usize) -> usize)),
+            _ => None,
         });
 
-    ops.zip_eq(numbers)
-        .map(|(op, nums)| nums.into_iter().reduce(op).unwrap())
-        .sum()
+    let width = numbers.first().unwrap().len();
+
+    ops.map(|(col, op)| {
+        (col..width)
+            .map(|col| String::from_utf8(numbers.iter().map(|row| row[col]).collect_vec()).unwrap())
+            .take_while(|col| !col.trim().is_empty())
+            .map(|col| col.trim().parse().unwrap())
+            .reduce(op)
+            .unwrap()
+    })
+    .sum()
 }

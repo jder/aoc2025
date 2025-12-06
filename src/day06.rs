54d5232a1ae659dc75a3d913dd94282154c094de
Jesse Rusak <me@jesserusak.com>
2025-12-06 10:59:47 -0500

    broken transpose needs spaces to newlines

--- src/day06.rs
+++ src/day06.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, Mul};
+use std::{
+    iter,
+    ops::{Add, Mul},
+};
 
 #[allow(unused)]
 use crate::prelude::*;
@@ -28,6 +31,43 @@ pub fn part1(input: &str, _is_sample: bool) -> usize {
         .sum()
 }
 
+fn transpose<'l>(lines: impl Iterator<Item = &'l str>) -> impl Iterator<Item = String> {
+    let lines = lines.map(|l| l.as_bytes()).collect_vec();
+    (0..lines.first().unwrap().len()).map(move |index| {
+        String::from_utf8(
+            lines
+                .iter()
+                .map(|line| line[index])
+                .chain(iter::once(' ' as u8))
+                .collect_vec(),
+        )
+        .unwrap()
+    })
+}
+
 pub fn part2(input: &str, _is_sample: bool) -> usize {
-    todo!()
+    let mut lines = input.lines();
+    let numbers = transpose(lines.take_while_ref(|line| !line.starts_with(&['*', '+'])))
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    println!("{numbers:?}");
+
+    let ops = lines
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|c| match c {
+            "+" => <usize as Add>::add,
+            "*" => <usize as Mul>::mul,
+            _ => panic!("unknown op {c}"),
+        });
+
+    ops.zip_eq(numbers)
+        .map(|(op, nums)| nums.into_iter().reduce(op).unwrap())
+        .sum()
 }

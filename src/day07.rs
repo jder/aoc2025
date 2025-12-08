bc0caab16583e2a5e58c32245841c054a6ca99bb
Jesse Rusak <me@jesserusak.com>
2025-12-07 21:06:35 -0500

    day 7 part 2

--- src/day07.rs
+++ src/day07.rs
@@ -39,6 +39,31 @@ pub fn part1(input: &str, _is_sample: bool) -> usize {
     count_splits(start)
 }
 
+fn count_timelines<'a>(
+    start: Option<Cell<'a, char>>,
+    cache: &mut HashMap<Cell<'a, char>, usize>,
+) -> usize {
+    let Some(start) = start else { return 1 };
+
+    if let Some(result) = cache.get(&start) {
+        *result
+    } else {
+        let result = if *start.contents() == '^' {
+            count_timelines(start.offset(-1, 1), cache) + count_timelines(start.offset(1, 1), cache)
+        } else {
+            count_timelines(start.offset(0, 1), cache)
+        };
+        cache.insert(start, result);
+
+        result
+    }
+}
+
 pub fn part2(input: &str, _is_sample: bool) -> usize {
-    todo!()
+    let grid = Grid::new_with_lines(input.lines());
+
+    let start = grid.cells().find(|cell| *cell.contents() == 'S');
+
+    let mut cache = HashMap::new();
+    count_timelines(start, &mut cache)
 }

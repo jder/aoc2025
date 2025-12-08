88b0770313845b16b36eeaf42d25c8e31f4992c0
Jesse Rusak <me@jesserusak.com>
2025-12-08 10:05:07 -0500

    part 2

--- src/day08.rs
+++ src/day08.rs
@@ -5,17 +5,22 @@ use euclid::point3;
 #[allow(unused)]
 use crate::prelude::*;
 
-fn join(a: usize, b: usize, subgraph_roots: &mut Vec<usize>) {
+// Returns size of new circuit
+fn join(a: usize, b: usize, subgraph_roots: &mut Vec<usize>) -> usize {
     let (new, old) = if a < b { (a, b) } else { (b, a) };
+    let mut new_size = 0;
     for root in subgraph_roots {
         if *root == old {
-            *root = new
+            *root = new;
+            new_size += 1;
+        } else if *root == new {
+            new_size += 1;
         }
     }
+    new_size
 }
 
-pub fn part1(input: &str, is_sample: bool) -> usize {
-    let num_joins = if is_sample { 10 } else { 1000 };
+fn parse(input: &str) -> (Vec<Point3D<isize>>, Vec<(isize, usize, usize)>) {
     let points: Vec<Point3D<isize>> = input
         .lines()
         .map(|line| {
@@ -35,6 +40,13 @@ pub fn part1(input: &str, is_sample: bool) -> usize {
         .flat_map(|((i0, p0), (i1, p1))| (i0 < i1).then(|| ((*p0 - *p1).square_length(), i0, i1)))
         .sorted()
         .collect_vec();
+
+    (points, distances)
+}
+
+pub fn part1(input: &str, is_sample: bool) -> usize {
+    let num_joins = if is_sample { 10 } else { 1000 };
+    let (points, distances) = parse(input);
 
     let mut subgraph_roots = (0..points.len()).collect_vec();
     for (_dist, a, b) in distances.into_iter().take(num_joins) {
@@ -57,5 +69,17 @@ pub fn part1(input: &str, is_sample: bool) -> usize {
 }
 
 pub fn part2(input: &str, _is_sample: bool) -> usize {
-    todo!()
+    let (points, distances) = parse(input);
+
+    let mut subgraph_roots = (0..points.len()).collect_vec();
+    for (_dist, a, b) in distances.into_iter() {
+        let root_a = subgraph_roots[a];
+        let root_b = subgraph_roots[b];
+        if root_a != root_b {
+            if join(root_a, root_b, &mut subgraph_roots) == points.len() {
+                return (points[a].x * points[b].x) as usize;
+            }
+        }
+    }
+    panic!("didn't make full circuit")
 }
